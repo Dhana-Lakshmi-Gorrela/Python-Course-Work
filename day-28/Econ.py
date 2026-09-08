25230d6673cf1ecce73c83534b56741ac40eb952
Dhana-Lakshmi-Gorrela <dhanalakshmigorrela3@gmail.com>
2026-09-08 16:20:56 +0530

    econ

---
+++ day-28/Econ.py
@@ -0,0 +1,25 @@
+'''
+base_premium = 10,000
+
+age = input().strip()
+Health_score= int(input())
+vehicle_type = input().strip()
+
+if age < 25:
+    age_premium *= 1.2
+elif age > 50:
+    age *= 1.5
+
+elif health_score > 80:
+    health_premium *= 1.0
+elif health_score < = 60:
+    health *= 0
+
+if vehicle_type == "spots car":
+    premium += premium * 0.30:
+
+elif vehicle_type = "SUV":
+    premium += premium *0.15
+
+
+
