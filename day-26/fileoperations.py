78925cd6c0cd1eae53de31e4829eabfdb9ac7e46
Dhana-Lakshmi-Gorrela <dhanalakshmigorrela3@gmail.com>
2026-09-08 16:15:06 +0530

    fileoperations

---
+++ day-26/fileoperations.py
@@ -0,0 +1,33 @@
+'''
+file = open('pfs-63.txt', 'r')
+print(file.read())
+
+file.seek(0)
+print(file.readline())
+
+file.seek(0)
+print(file.readlines())
+
+file.close()
+'''
+'''
+with open('pfs-63.txt','r') as file:
+    print(file.read())
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())
+
+'''
+'''
+with open('msql.txt','w') as file:
+    file.write("DDL,DML,DCL")
+'''   
+'''
+with open('pfs-63.txt','a') as file:
+    file.write("only for today")
+'''
+with open('pfs-63.txt','a+') as file:
+    file.write("Tom same branch 5")
+    file.seek(0)
+    print(file.read())
