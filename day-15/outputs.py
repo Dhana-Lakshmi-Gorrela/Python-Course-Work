972ca20d85d3ff78e45c67d9a015009fb41523a8
Dhana-Lakshmi-Gorrela <dhanalakshmigorrela3@gmail.com>
2026-09-08 15:59:07 +0530

    assert_pass

---
+++ day-15/outputs.py
@@ -0,0 +1,77 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+01234
+12345
+23456
+34567
+45678
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+01010
+01010
+01010
+01010
+01010
+
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+01010
+10101
+01010
+10101
+01010
+>>> 
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+>>> 
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+x
+xx
+xxx
+xxxx
+xxxxx
+>>> 
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+>>> 
+= RESTART: C:/Users/ADMIN/OneDrive/Documents/Desktop/python course work/day-15/assert_pass.py
+xxxxx
+xxxx
+xxx
+xx
+x
